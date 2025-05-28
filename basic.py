33452d7b0c75a43b4f067ebfea6da64533f22476
SinofPride-999 <jhaycodes999@gmail.com>
2025-05-28 23:13:09 +0000

    Working on the Logical and Comparison Ops

--- basic.py
+++ basic.py
@@ -19,6 +19,14 @@ TT_IDENTIFIER = 'IDENTIFIER'
 TT_KEYWORD = 'KEYWORD'
 TT_EQ = 'EQ' 
 
+# COMPARISONS
+TT_EE = 'EE'
+TT_NE = 'NE'
+TT_LT = 'LT'
+TT_GT = 'GT'
+TT_LTE = 'LTE'
+TT_GTE = 'GTE'
+
 # AITHMETICS
 TT_PLUS = 'PLUS'
 TT_MINUS = 'MINUS'
@@ -33,7 +41,10 @@ TT_RPAREN = 'RPAREN'
 TT_EOF = 'EOF'  # To detect if we reach EOF in Parser
 
 KEYWORDS = [
-    'initiate'
+    'initiate',
+    'AND',
+    'OR',
+    'NOT'
 ]
 
 #################################
@@ -56,6 +67,10 @@ class Error:
 class IllegalCharError(Error):
     def __init__(self, pos_start, pos_end, details):
         super().__init__(pos_start, pos_end, 'Illegal Character', details)
+
+class ExpectedCharError(Error):
+    def __init__(self, pos_start, pos_end, details):
+        super().__init__(pos_start, pos_end, 'Expected Character', details)
 
 class InvalidSyntaxError(Error): 
     def __init__(self, pos_start, pos_end, details):
@@ -180,15 +195,24 @@ class Lexer:
             elif self.current_char == '^':
                 tokens.append(Token(TT_POW, pos_start = self.pos))
                 self.advance()
-            elif self.current_char == '=':
-                tokens.append(Token(TT_EQ, pos_start = self.pos))
-                self.advance()
             elif self.current_char == '(':
                 tokens.append(Token(TT_LPAREN, pos_start = self.pos))
                 self.advance()
             elif self.current_char == ')':
                 tokens.append(Token(TT_RPAREN, pos_start = self.pos))
                 self.advance()
+            elif self.current_char == '!':
+                tok, error = self.make_not_equals()
+
+                if error:
+                    return [], error
+                tokens.append(tok)
+            elif self.current_char == '=':
+                tokens.append(self.make_equals())
+            elif self.current_char == '<':
+                tokens.append(self.make_less_than())
+            elif self.current_char == '>':
+                tokens.append(self.make_greater_than())
             else:
                 pos_start = self.pos.copy()
                 char = self.current_char
@@ -231,6 +255,54 @@ class Lexer:
 
         tok_type = TT_KEYWORD if id_str in KEYWORDS else TT_IDENTIFIER
         return Token(tok_type, id_str, pos_start, self.pos)
+
+
+    def make_not_equals(self):
+        pos_start = self.pos.copy()
+        self.advance()
+
+        if self.current_char == '=':
+            self.advance()
+            return Token(TT_NE, pos_start = pos_start, pos_end = self.pos), None
+        
+        self.advance()
+        return None, ExpectedCharError(
+            pos_start, self.pos,
+            "Expected '=', (after '!')"
+        )
+    
+    def make_equals(self):
+        tok_type = TT_EQ
+        pos_start = self.pos.copy()
+        self.advance()
+
+        if self.current_char == '=':
+            self.advance()
+            tok_type = TT_EE
+
+        return Token(tok_type, pos_start = pos_start, pos_end = self.pos)
+    
+    def make_less_than(self):
+        tok_type = TT_LT
+        pos_start = self.pos.copy()
+        self.advance()
+
+        if self.current_char == '=':
+            self.advance()
+            tok_type = TT_LTE
+
+        return Token(tok_type, pos_start = pos_start, pos_end = self.pos)
+    
+    def make_greater_than(self):
+        tok_type = TT_GT
+        pos_start = self.pos.copy()
+        self.advance()
+
+        if self.current_char == '=':
+            self.advance()
+            tok_type = TT_GTE
+
+        return Token(tok_type, pos_start = pos_start, pos_end = self.pos)
 
 #################################
 #   NODES
@@ -546,7 +618,7 @@ class Number:
 
         copy.set_pos(self.pos_start, self.pos_end)
         copy.set_context(self.context)
-        
+
         return copy
 
     def __repr__(self):
